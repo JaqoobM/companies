1181588767412f67eefdc3c7f24e5ba1d5e82cb1
Jakub <jm54706@outlook.com>
2023-09-19 21:25:53 +0200

    views fixed

--- app/db/validators.js
+++ app/db/validators.js
@@ -1,14 +1,12 @@
 module.exports = {
-     checkForbidenString(value, forbidenString) {
-          if (value === forbidenString) {
-               throw new Error('Nazwa "slug" jest zakazana');
-          }
-     },
+	checkForbidenString(value, forbidenString) {
+		if (value === forbidenString) {
+			throw new Error('Nazwa "slug" jest zakazana');
+		}
+	},
 
-     validateEmail(email) {
-          if (value === forbidenString) {
-               var re = /^\w+([/.-]?w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-               return re.test(email)
-          }
-     }
-}
+	validateEmail(email) {
+		var re = /^\w+([/.-]?w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		return re.test(email);
+	},
+};
